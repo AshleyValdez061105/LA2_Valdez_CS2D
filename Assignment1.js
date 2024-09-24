8cd6c816d2f044ebeab25792725bf48e2ff62378
AshleyValdez061105 <ashleyvaldez061105@gmail.com>
2024-09-24 08:05:45 +0800

    Assignment1.js

---
+++ Assignment1.js
@@ -0,0 +1,38 @@
+// C1-A1
+
+// Persona 1, Student Information (Me)
+let fullName = "Ashley H Valdez "
+let birthdate = "June 11 , 2005";
+let birthplace = "Conconig East sta. Lucia Ilocos Sur";
+let address = "Conconig East sta. Lucia Ilocos Sur ";
+let courseAndYear = "Bachelor Of Science In Computer Science, 2nd Year";
+let dreamJobAfterGraduation = "software engineering";
+
+// Persona 2, 1st Classmate (this is a random name, and identity)
+let classmate1FullName = "sharmine anne Moreno"
+let classmate1Birthdate = "October 05, 2005";
+let classmate1Birthplace = "Paratong, sta. Lucia, Ilocos Sur";
+let classmate1Address = "Paratong, sta. Lucia, Ilocos Sur";
+let classmate1CourseAndYear = "Bachelor Of Science In Computer Science, 2nd Year";
+let classmate1DreamJob = "programer";
+
+// Persona 3, 3rd Classmate (also a random)
+let classmate2FullName = "Jolly anne Estacio"
+let classmate2Birthdate = "October 31, 2005";
+let classmate2Birthplace = "Conconig East, sta. Lucia Ilocos Sur";
+let classmate2Address = "Conconig East, sta. Lucia Ilocos Sur";
+let classmate2CourseAndYear = "Bachelor Of Science In Nursing  , 2nd Year";
+let classmate2DreamJob = "Nursing";
+
+// Print information for Student 1 (Me)
+let student1Info = "[" + fullName.toUpperCase() + "] was born on [" + birthdate + "] at [" + birthplace.toUpperCase() + "], and currently living at [" + address.toUpperCase() + "]. [" + fullName.toLowerCase() + "] is taking up [" + courseAndYear + "] and dreams to be [" + dreamJobAfterGraduation + "] after graduation.\n";
+console.log(student1Info);
+
+// Print information for Classmate 1
+let classmate1Info = "[" + classmate1FullName + "] was born on [" + classmate1Birthdate + "] at [" + classmate1Birthplace.toUpperCase() + "], and currently living at [" + classmate1Address.toUpperCase() + "]. [" + classmate1FullName.toLowerCase() + "] is taking up [" + classmate1CourseAndYear + "] and dreams to be [" + classmate1DreamJob + "] after graduation.\n";
+console.log(classmate1Info);
+
+// Print information for Classmate 2
+let classmate2Info = "[" + classmate2FullName + "] was born on [" + classmate2Birthdate + "] at [" + classmate2Birthplace.toUpperCase() + "], and currently living at [" + classmate2Address.toUpperCase() + "]. [" + classmate2FullName.toLowerCase() + "] is taking up [" + classmate2CourseAndYear + "] and dreams to be [" + classmate2DreamJob + "] after graduation.";
+console.log(classmate2Info);
+  
